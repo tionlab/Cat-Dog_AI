c52ad8d1c25100be26d76c2dd3dfc1b9adfcdbe0
TATION <96953236+tionlab@users.noreply.github.com>
2023-03-09 13:12:14 +0900

    Add files via upload

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip install tensorflow")
 os.system("pip install tkinter")
-os.system("pip install PIL")
+os.system("pip install Pillow")
 os.system("pip install tensorflow")
 os.system("pip install keras_preprocessing")
